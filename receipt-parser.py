e9a9c47a7ca176b586243441f8c39320f2bebbbb
Krish Shah <shahkrish2016@gmail.com>
2024-01-02 00:11:44 -0500

    added receipt parsing functionality

--- receipt-parser.py
+++ receipt-parser.py
@@ -1,32 +1,56 @@
 from dotenv import load_dotenv
-
-class ReceiptParser:
-    pass
-
-prompt = """This list shows text extracted from a paper receipt. Return in JSON format the vendor where the purchase occurred, the product purchased, the date of purchase, and the cost of that service"""
-
+import json
 from openai import OpenAI
 import os
 
+# loading API
 load_dotenv()
 
+# initializing OpenAI client
 client = OpenAI(
     # This is the default and can be omitted
     api_key= os.getenv("API_KEY"),
 )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "how do you know everything",
-        }
-    ],
-    model="gpt-3.5-turbo",
-)
+# class ReceiptParser:
+#     pass
 
-# chat completion object
-completion_obj = chat_completion.choices[0].message
+def parse_receipt(aws_response):
+    """Parses receipt and returns JSON dictionary 
 
-# get content
-print(completion_obj.content)
+    :param aws_response: a AWS Rekognition result
+    :type aws_response: JSON dictionary
+    :return: a list of items contained in the receipt
+    :rtype: JSON dictionary
+    """
+
+    prompt = "This list shows text extracted from a paper receipt.\n"
+
+    # clean up response to only include text content
+    aws_response = [elem['DetectedText'] for elem in a['TextDetections']]
+
+    # building a list of all of the text items on the receipt
+    receipt_list = ""
+    for item in aws_response:
+        receipt_list += f"- {item}\n"
+
+    prompt += receipt_list + "\nReturn in JSON format the vendor where each purchase occurred, the product purchased, the date of purchase, and the cost of that service. Do this for every single product on the receipt."
+
+    gpt_response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+
+    message = gpt_response.choices[0].message
+    return json.loads(message.content)
+
+# loading sample rekognition response to test parse_receipt
+with open('aws-sample-response2.json', 'r') as f:
+    a = json.load(f)
+    
+print(parse_receipt(a))
