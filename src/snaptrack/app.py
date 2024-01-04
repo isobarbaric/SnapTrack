ec92413e1ec348e234843c0c82f387af695ed9d0
Krish Shah <shahkrish2016@gmail.com>
2024-01-03 20:59:06 -0500

    separated selection columns from non-selection columns, work in progress

--- src/snaptrack/app.py
+++ src/snaptrack/app.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 from snaptrack.receipt_parser import ReceiptParser
 from snaptrack.notion import NotionDB
+import time
 
 load_dotenv()
 
@@ -9,6 +10,8 @@ notion_token = os.environ["NOTION_TOKEN"]
 database_id = os.environ["NOTION_DATABASE_ID"]
 
 def main():
+    start = time.time()
+
     db = NotionDB(notion_token, database_id)
     receipt_parser = ReceiptParser()
     # print(db.columns)
@@ -17,13 +20,16 @@ def main():
     products = receipt_parser.parse(filepath = "../../data/receipts/receipt3.jpg", 
                                     categories = db.columns, 
                                     select_options = db.select_options)
-    # print(products)
+    print(products)
 
-    select_fields = db.select_options.keys()
+    # select_fields = db.select_options.keys()
     # print(select_fields)
-    for product in products:
-        # print(product)
+    # for product in products:
+    #     print(product)
     #     db.add_row(product)
+
+    end = time.time()
+    print(f'\n{end - start} seconds elapsed')
 
 if __name__ == '__main__':
     main()
--- src/snaptrack/receipt_parser.py
+++ src/snaptrack/receipt_parser.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import json
 from openai import OpenAI
 import os
+import time
 
 # loading api key
 load_dotenv()
@@ -58,12 +59,112 @@ class ReceiptParser:
         :return: a list of items contained in the receipt
         :rtype: JSON dictionary
         """
-
-        # created a detailed prompt for task
-        prompt = "Train: You are an expert in exploratory data extraction. You have award-winning proficiency in identifying and collecting relevant information from the text extracted from paper receipts. Based on your extensive experience and expertise, analyze a receipt's text list. There are labels that represent columns in a Notion database. Scrutinize all extracted text for each entry in the receipt and assign them to appropriate labels (don't create your own labels, only create keys for given labels). Some of the columns of the database are of type 'Select' and type 'Multi-select' and hence have associated categories. For such columns, please do not mix categories defined within one label with another. For a particular product, assign a label an empty string if you are unsure what value should be assigned, but make sure to ALWAYS include every label for a particular entry. Please include dates in %Y/%m/%d format excluding time, and correct the content in a title word format. Your output should ONLY be a list of JSON objects and nothing else. This list is text extracted from a paper receipt: "
-
         # clean up response to only include text content
-        aws_response = [elem['DetectedText'] for elem in aws_response['TextDetections']]
+        aws_text = [elem['DetectedText'] for elem in aws_response['TextDetections']]
+
+        # building a list of all of the text items on the receipt
+        receipt_list = "["
+        for item in aws_text[:-1]:
+            receipt_list += f"{item}, "
+        receipt_list += f"{aws_text[-1]}]"
+        
+        # print(prompt)
+        # passing prompt to GPT-3.5 and gettings its response
+        purchases = self.process_non_select_cols(receipt_list, categories)
+
+        select_column_names = select_options.keys()
+        print(purchases)
+
+        # loop through individual select columns and add key to each page
+        # code here
+
+        return purchases
+
+    def get_gpt_response(self, prompt):
+        gpt_response = openai_client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            model="gpt-3.5-turbo"
+        )
+
+        message = gpt_response.choices[0].message
+        # print(message)
+
+        try:
+            # details = json.loads(message)
+            details = json.loads(message.content)
+        except json.decoder.JSONDecodeError as e:
+            details = {'Error': e}
+
+        return details
+
+    def process_non_select_cols(self, receipt_list, categories):
+        # created a detailed prompt for task
+        prompt = "There are labels that represent columns in a Notion database. Scrutinize all extracted text for each entry in the receipt and assign them to appropriate labels (don't create your own labels, only create keys for given labels). For a particular product, assign a label an empty string if you are unsure what value should be assigned, but make sure to ALWAYS include every label for a particular entry. Please include dates in %Y/%m/%d format excluding time, and correct the content in a title word format. Note that each entry will have the same date that should be a part of the receipt. Your output should ONLY be a list of JSON objects and nothing else. Don't list payment details, vendor details as separate purchases. This list is text extracted from a paper receipt: "
+
+        # adding items on receipt to prompt
+        prompt += receipt_list
+
+        columns = ''
+        for column in categories:
+            if column['type'] in ['select', 'multi_select']:
+                continue
+            columns += f"\n- {column['name']} "
+
+        prompt += columns
+        # print(prompt)
+
+        # remove those with empty 
+        # count length
+        # sort, choose elem with minimum length
+
+        batches = []
+        for i in range(1, 4):
+            # processing one batch of responses
+            # print(f'Batch #{i}')
+            current_batch = []
+
+            try:
+                gpt_json = self.get_gpt_response(prompt)
+            except Exception as e:
+                pass
+
+            for entry in gpt_json:
+                curr_entry = 0
+                for key, value in entry.items():
+                    if value == '':
+                        curr_entry += 1
+                if curr_entry <= int(len(categories)/2):
+                    current_batch.append(entry)
+            
+            batches.append([len(current_batch), current_batch])
+            # print(batches[-1])
+            # print('\n')
+            time.sleep(1)
+    
+        # print(batches)
+
+        # batches.sort()
+        min_batch_size = 100000
+        best_batch = None
+        for batch in batches:
+            if batch[0] < min_batch_size:
+                min_batch_size = batch[0]
+                best_batch = batch[1]
+        
+        # remove lists with empty length from batches.append
+        # first figure out why getting zero length
+
+        # choose between similar length lists
+
+        return best_batch
+ 
+    def process_select_columns(self, aws_response, categories, select_options = None):
+        prompt = "Some of the columns of the database are of type 'Select' and type 'Multi-select' and hence have associated categories. For such columns, please do not mix categories defined within one label with another."
 
         # building a list of all of the text items on the receipt
         receipt_list = "["
@@ -92,30 +193,6 @@ class ReceiptParser:
                     criteria += ')'
         
         prompt += receipt_list + f"\nYour labels are {criteria}"
-
-        # print(prompt)
-
-        # passing prompt to GPT-3.5 and gettings its response
-        gpt_response = openai_client.chat.completions.create(
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
-            ],
-            model="gpt-3.5-turbo"
-        )
-
-        message = gpt_response.choices[0].message
-
-        try:
-            # details = json.loads(message)
-            details = json.loads(message.content)
-        except json.decoder.JSONDecodeError as e:
-            details = {'Error': e}
-
-        # print(details)
-        return details
 
     def parse(self, filepath, categories, select_options):
         rekognition_response = self.get_rekognition_response(filepath)
