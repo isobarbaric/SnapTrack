ded64c2b608c8e90a24143979c21b3fbee9c1197
Krish Shah <shahkrish2016@gmail.com>
2024-01-05 14:36:06 -0500

    removed temp directory

--- test/test_receipt_parser.py
+++
@@ -1,8 +0,0 @@
-import json
-from snaptrack.receipt_parser import ReceiptParser
-
-a = ReceiptParser()
-data = a.parse(filepath='../data/receipts/receipt1.jpg', columns=['Date', 'Place', 'What', 'Amount'])
-
-with open('../data/purchase-data.json', 'w') as file:
-    json.dump(data, file, indent=4)    
